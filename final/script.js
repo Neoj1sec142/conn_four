35c9413fde34b41438b9f4216a71a6a7a33ade51
Mark Harmon <neothej1sec@gmail.com>
2022-03-08 13:38:20 -0600

    cleaning up repo file

--- final/script.js
+++ final/script.js
@@ -75,14 +75,13 @@ document.addEventListener('DOMContentLoaded', () => {
     /////////////////////
     //adding event listener to my reset button
     resetB.addEventListener('click',reload, false);
-    //adding a for loop to run through each sq
+    //using a for loop to run through each sq
     for(let i = 0; i < allsq.length; i++){
         //adding onclick eventlistener for all squares
         allsq[i].onclick = () => {
             //if else statement to run game 
             //you can only choose a square if the one below = invaild
             //invalid meaning the square is already full
-            
             if (allsq[i + 7].classList.contains('invalid')){
                 //if current player = pl1
                 if(cpl == 1){
