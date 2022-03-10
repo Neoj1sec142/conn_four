e13007ef4ceeba120a6bf8c221bbf3a7653815c7
Mark Harmon <neothej1sec@gmail.com>
2022-03-10 13:25:29 -0600

    trying to establish mode button functionality

---
+++ lscript.js
@@ -0,0 +1,10 @@
+const light = document.querySelector('#light')
+const dark = document.querySelector('#dark')
+
+function lighten () {
+    document.body.style.backgroundColor = 'white';
+}
+function darken () {
+    document.body.style.backgroundColor = ""; 
+}
+
