97bbc8d1fb4bd300f5005d4c18fcbeafd78f2ab4
Mark Harmon <neothej1sec@gmail.com>
2022-03-10 15:17:20 -0600

    working on buttons still

--- lscript.js
+++ lscript.js
@@ -1,10 +1,23 @@
-const light = document.getElementById('light')
-const dark = document.getElementById('dark')
-const body = document.body
-function lighten () {
-    body.style.backgroundColor = 'white';
-}
-function darken () {
-    body.style.backgroundColor = ""; 
-}
 
+document.addEventListener('DOMContentLoaded', () => {
+    const light = document.getElementById('light');
+    const dark = document.getElementById('dark');
+    const message = document.querySelectorAll('.message')
+    
+    function darkMode() {
+        message.style.opacity
+    }
+    
+    
+    dark.addEventListener('click', () =>{
+        darkMode
+        alert('dark mode')
+    })
+    light.addEventListener('click', () =>{
+        // document.body.style.backgroundImage = null;
+        document.body.style.background = white;
+        alert('light mode');
+    })
+    
+    
+})
